fix units

nagl/datasets.py
@@ -6,6 +6,7 @@ import typing
 import dgl
 import pyarrow.parquet
 import torch
+import polars as pl
 from rdkit import Chem
 from torch.utils.data import Dataset
 
@@ -218,10 +219,11 @@ class DGLMoleculeDataset(Dataset):
 
         required_columns = ["smiles", "atom_features", "bond_features"]
         columns = None if columns is None else required_columns + columns
-
-        table = pyarrow.parquet.read_table(paths, columns=columns)
-
-        label_list = table.to_pylist()
+        
+        # table = pyarrow.parquet.read_table(paths, columns=columns)
+        table = pl.scan_parquet(paths)#, columns=columns)
+        
+        label_list = table.collect().to_dicts()
         label_list = (
             label_list if progress_iterator is None else progress_iterator(label_list)
         )